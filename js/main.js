55fc3bbf6de1b405ed99802b55e3e0c9bcf43c2b
Mac <mac@MacBook-Air-Mac.local>
2018-09-04 12:05:48 +0400

    begin to render user list

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ var USERS_SRC = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,
 var TIME_OUT = 15000;
 
 
-/** Class UserStorage - load,render,sort the userlist
+/** 
+ * Class UserStorage - load,render,sort the userlist
  * 
  * @param string url - link  
  */
@@ -57,36 +58,10 @@ function UserStorage(url)
 //     doPokemonsClick(pokemons);
 // }
 
-// // этот метод использовать по конкретному покемону
-// PokemonStorage.prototype.loadExtraData = function(loadResults) {
-//     //
-//     //Сделать перебор элементов массива, и по каждому
-
-//     var xhr = new XMLHttpRequest();
-//     var url = loadResults['results'][4].url;
-//     xhr.open('GET', url, true);
-//     xhr.timeout = TIME_OUT;
-//     xhr.ontimeout = function() {
-//         console.log('Time is up');
-//         var container = document.getElementById('container');
-//         container.innerHTML = '<h1 class="error"> Pokemons don`t loaded, please press F5 for reload the page</>';
-//     }
-//     xhr.send();
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === XMLHttpRequest.DONE) {
-//             if (xhr.status === 200) {
-//                 var lResults = JSON.parse(xhr.responseText);
-//                 //Передать результаты на отрисовку DOM элементов
-//                 //context.renderPokemons(loadResults);
-//                 console.log(lResults);
-//             }
-//         }
-//     }
-// }
 
 
-
-/** UserStorage method - for load user data from url
+/** 
+ * UserStorage method - for load user data from url
  * 
  */
 UserStorage.prototype.loadUsers = function() 
@@ -116,13 +91,13 @@ UserStorage.prototype.loadUsers = function()
     }
 }
 
-/** UserStorage method - create objects "User" for each item and push them into class property (array of objects)
+/** 
+ * UserStorage method - create objects "User" for each item and push them into class property (array of objects)
  * 
  * @param array results 
  */
 UserStorage.prototype.addToStorage = function(results)
 {
-   // console.log(this.userList);
     this.userList = [];
     var uList = this.userList;
     results['results'].forEach(function(item,index) 
@@ -145,9 +120,71 @@ UserStorage.prototype.addToStorage = function(results)
         );
         uList.push(usr);
     });
-    console.log(this.userList);
+    //console.log(this.userList);
+    this.renderList();
 }
 
+UserStorage.prototype.renderList = function()
+{
+    var DOM_userList = document.getElementById('userList');
+
+    this.userList.forEach(function(item,index,array)
+    {
+        // create DOM elements
+        var DOM_item = document.createElement('div');
+        var DOM_picture = document.createElement('img');
+        var DOM_title = document.createElement('div');
+        var DOM_fName = document.createElement('div');
+        var DOM_lName = document.createElement('div');
+        // get data from User object
+        var picture = item.mediumPic;
+        var title = item.title;
+        var fName = item.fName;
+        var lName = item.lName;
+        var id = item.id;
+        // add class for DOM elements
+        DOM_item.classList.add('listItem');
+        DOM_picture.classList.add('listItem__picture');
+        DOM_title.classList.add('listItem__title');
+        DOM_fName.classList.add('listItem__fName');
+        DOM_lName.classList.add('listItem__lName');
+        // set data in DOM elements
+        DOM_picture.src = picture;
+        DOM_title.innerHTML = title;
+        DOM_fName.innerHTML = fName;
+        DOM_lName.innerHTML = lName;
+        // create DOM structure
+        DOM_userList.appendChild(DOM_item);
+        DOM_item.appendChild(DOM_picture);
+        DOM_item.appendChild(DOM_title);
+        DOM_item.appendChild(DOM_fName);
+        DOM_item.appendChild(DOM_lName);
+
+
+
+
+    })
+}
+
+
+/** 
+ * Class User - render userCard
+ * 
+ * @param int id 
+ * @param string gender 
+ * @param string email 
+ * @param string city 
+ * @param string postcode 
+ * @param string state 
+ * @param string street 
+ * @param string title 
+ * @param string fName 
+ * @param string lName 
+ * @param string phone 
+ * @param string smallPic 
+ * @param string mediumPic 
+ * @param string largePic 
+ */
 function User(id,gender,email,city,postcode,state,street,title,fName,lName,phone,smallPic,mediumPic,largePic)
 {
     this.id = id;
@@ -157,8 +194,8 @@ function User(id,gender,email,city,postcode,state,street,title,fName,lName,phone
     this.postcode = postcode;
     this.state = state;
     this.street = street;
-    this. title = title;
-    this. fName = fName;
+    this.title = title;
+    this.fName = fName;
     this.lName = lName;
     this.phone = phone;
     this.smallPic = smallPic;
@@ -167,5 +204,7 @@ function User(id,gender,email,city,postcode,state,street,title,fName,lName,phone
 }
 
 
+
 var uStorage = new UserStorage(USERS_SRC);
 uStorage.loadUsers();
+//uStorage.renderList();
